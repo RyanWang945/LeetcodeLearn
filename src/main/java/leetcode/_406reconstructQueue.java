b8ec2416b4ef419f8090deb81f0b3f77ee05e4ed
RyanWang945 <RyanWang1984z@gmail.com>
2020-02-29 12:58:05 +0800

    三道greedy

---
+++ src/main/java/leetcode/_406reconstructQueue.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Ryan
+ * @date 2020/2/29 9:32
+ */
+public class _406reconstructQueue {
+    public int[][] reconstructQueue(int[][] people) {
+        if(people==null||people.length==0||people[0].length==0){
+            return new int[0][0];
+        }
+        Arrays.sort(people,(a,b)->(a[0]==b[0]?a[1]-b[1]:b[0]-a[0]));
+        List<int[]>queue=new ArrayList<>();
+        for(int[] p:people){
+            queue.add(p[1],p);
+        }
+        return queue.toArray(new int[queue.size()][]);
+    }
+}
---
+++ src/main/java/leetcode/_452findMinArrowShots.java
@@ -0,0 +1,52 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * @author Ryan
+ * @date 2020/2/29 9:11
+ */
+public class _452findMinArrowShots {
+    public int findMinArrowShots(int[][] points) {
+        if(points.length==0){
+            return 0;
+        }
+        Comparator c=new MyComparator();
+        Integer[][] newPoints = new Integer[points.length][2];
+        for(int i=0;i<points.length;i++){
+            for(int j=0;j<2;j++){
+                newPoints[i][j]=points[i][j];
+            }
+        }
+        Arrays.sort(newPoints,c);
+        int start=newPoints[0][0];
+        int end=newPoints[0][1];
+        int count=1;
+        int minEnd=end;
+        for(int i=1;i<newPoints.length;i++){
+            if(newPoints[i][0]>=start&&newPoints[i][1]<=minEnd){
+                minEnd=newPoints[i][1];
+                continue;
+            }else{
+                count++;
+                start=newPoints[i][0];
+                end=newPoints[i][1];
+            }
+        }
+        return count;
+    }
+}
+class MyComparator implements Comparator<Integer[]> {
+
+
+    @Override
+    public int compare(Integer[] o1, Integer[] o2) {
+        if(o1[0]<o2[0]){
+            return -1;
+        }else if(o1[0]>o2[0]){
+            return 1;
+        }else
+            return 0;
+    }
+}
